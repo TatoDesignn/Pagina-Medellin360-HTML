dc1a53d08662e0f49e160cc905d10cca9956f3d0
TatoDesign <santi1404@hotmail.com>
2024-02-07 22:50:19 -0500

    Primera interfaz y mapa

---
+++ js/map.js
@@ -0,0 +1,45 @@
+const map = L.map('map'); 
+
+map.setView([6.2427, -75.5745], 13); 
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map); 
+
+
+let marker, circle, zoomed;
+
+function success(pos) {
+
+    const lat = pos.coords.latitude;
+    const lng = pos.coords.longitude;
+    const accuracy = pos.coords.accuracy;
+
+    if (marker) {
+        map.removeLayer(marker);
+        map.removeLayer(circle);
+    }
+
+    marker = L.marker([lat, lng]).addTo(map);
+    circle = L.circle([lat, lng], { radius: accuracy }).addTo(map);
+
+    if (!zoomed) {
+        zoomed = map.fitBounds(circle.getBounds()); 
+    }
+
+    map.setView([lat, lng]);
+
+}
+
+function error(err) {
+
+    if (err.code === 1) {
+        alert("Please allow geolocation access");
+    } else {
+        alert("Cannot get current location");
+    }
+
+}
+
+navigator.geolocation.watchPosition(success, error);
--- js/script.js
+++ js/script.js
@@ -1,29 +1,15 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 18,
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  id: 'mapbox/streets-v11',
-  tileSize: 512,
-  zoomOffset: -1
-}).addTo(map);
+function verificacion(){
+    var email, password;
 
-function onLocationFound(e) {
-  var radius = e.accuracy / 2;
+    email = document.getElementById("email").value;
+    password = document.getElementById("password").value;
 
-  L.marker(e.latlng).addTo(map)
-    .bindPopup("Tu ubicación está aquí.").openPopup();
+    if(email == "medallo360@gmail.com" && password == "medallo360"){
+        window.location.href = "map.html";
+    }
+    else{
+        alert("Usuario o contraseña incorrecta.");
+    }
 
-  L.circle(e.latlng, radius).addTo(map);
-}
-
-function onLocationError(e) {
-  alert("No se pudo obtener tu ubicación. Asegúrate de que la geolocalización esté habilitada en tu navegador.");
-}
-
-map.on('locationfound', onLocationFound);
-map.on('locationerror', onLocationError);
-
-map.locate({setView: true, maxZoom: 16});
+ }
